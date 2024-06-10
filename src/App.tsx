3e261a36c10fa3c922a598f92171bd2eeea3cd8f
SpringDream0406 <springdream0406@gmail.com>
2024-06-10 14:52:14 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Main from "./pages/00.Main/Main";
+import SingUp from "./pages/01.SignUp/SingUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="signup" element={<SingUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
