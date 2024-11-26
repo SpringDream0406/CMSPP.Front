0399d6d2966ee6392583331f3eb9d4a09a2b2f94
SpringDream0406 <springdream0406@gmail.com>
2024-11-26 21:58:38 +0900

    test

--- src/pages/01.SignUp/SignUp.tsx
+++ src/pages/01.SignUp/SignUp.tsx
@@ -6,14 +6,13 @@ const Signup = () => {
   const signupIcons = signupIconsData.map((icons) => {
     return (
       <div className="signup-icons-box" key={icons}>
-        {/* <Link to={`${process.env.REACT_APP_BACK_URL}/signup/${icons}`}>
+        <Link to={`${process.env.REACT_APP_BACK_URL}/signup/${icons}`}>
           <img
             src={`/images/icons/${icons}.png`}
             alt={icons}
             className="signup-icons-icon"
           />
-        </Link> */}
-        <a href={`${process.env.REACT_APP_BACK_URL}/signup/google`}>google</a>
+        </Link>
       </div>
     );
   });
