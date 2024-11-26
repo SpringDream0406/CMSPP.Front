7bd346faa686392b61a85ceb18515b8acb24c773
SpringDream0406 <springdream0406@gmail.com>
2024-11-26 18:14:57 +0900

    요청으로

--- src/pages/01.SignUp/SignUp.tsx
+++ src/pages/01.SignUp/SignUp.tsx
@@ -1,18 +1,35 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
 import "./SignUp.css";
 
 const Signup = () => {
   const signupIconsData = ["google", "kakao", "naver"];
+
   const signupIcons = signupIconsData.map((icons) => {
+    const handleSignup = async () => {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACK_URL}/signup/${icons}`,
+          {},
+          {
+            withCredentials: true, // 쿠키 포함해서 보내기
+          }
+        );
+        // 성공적인 응답 처리 (예: 로그인 성공 후 페이지 리디렉션)
+        console.log(response.data);
+      } catch (error) {
+        console.error("Signup failed:", error);
+      }
+    };
+
     return (
       <div className="signup-icons-box" key={icons}>
-        <Link to={`${process.env.REACT_APP_BACK_URL}/signup/${icons}`}>
+        <button onClick={handleSignup}>
           <img
             src={`/images/icons/${icons}.png`}
             alt={icons}
             className="signup-icons-icon"
           />
-        </Link>
+        </button>
       </div>
     );
   });
